85e7bb6e812d87eb47934c221c336481dc97e954
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2025-04-22 16:19:20 +0700

    Update listperkalian.php

--- listperkalian.php
+++ listperkalian.php
@@ -9,9 +9,9 @@
     // List Perkalian
     for ($i = 1; $i <= 10; $i++) {
         for ($j = 1; $j <= 10; $j++) {
-        $hasil = $i * $j;
-        echo "<br>";
-        echo "$i x $j = $hasil";
+            $hasil = $i * $j;
+            echo "<br>";
+            echo "$i x $j = $hasil";
         }
         echo "<br>";
     }
