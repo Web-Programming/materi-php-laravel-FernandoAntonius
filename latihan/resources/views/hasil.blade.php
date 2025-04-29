dfba7423284dd5e3c38d0f6fadf168b3cd52ea87
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2025-04-29 15:12:26 +0700

    ControllerRouteView

--- latihan/resources/views/hasil.blade.php
+++ latihan/resources/views/hasil.blade.php
@@ -1,6 +1,9 @@
-<h1>Total dari
-<?php echo "$angka1 + $angka2 + $angka3 = "?> 
-<?php echo $angka1 + $angka2 + $angka3 ?></h1><br>
-@if ($angka3 == 0)
-    Bilangan ke 3 tidak diisi
-@endif
+<div>
+    <h1>Total dari
+    <?php echo "$angka1 + $angka2 + $angka3 = "?> 
+    <?php echo $angka1 + $angka2 + $angka3 ?></h1><br>
+
+    @if ($angka3 == 0)
+        Bilangan ke 3 tidak diisi
+    @endif
+</div>
