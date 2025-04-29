90b7f75f50f1757db810f7c7ee647a792152595b
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2025-04-29 15:00:46 +0700

    LatihanRoute

---
+++ latihan/resources/views/hasil.blade.php
@@ -0,0 +1,6 @@
+<h1>Total dari
+<?php echo "$angka1 + $angka2 + $angka3 = "?> 
+<?php echo $angka1 + $angka2 + $angka3 ?></h1><br>
+@if ($angka3 == 0)
+    Bilangan ke 3 tidak diisi
+@endif
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get("/berita/{id}/{title?}", function ($id, $title = NULL) {
+    return view('berita', ['id' => $id, 'title' => $title]);
+});
+
+Route::get('/total/{angka1?}/{angka2?}/{angka3?}', function ($angka1 = 0, $angka2 = 0, $angka3 = 0) {
+    return view('hasil', ['angka1' => $angka1, 'angka2' => $angka2, 'angka3' => $angka3]);
+});
